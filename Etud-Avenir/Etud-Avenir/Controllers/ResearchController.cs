05e1c9da5280f5bf8ae6e9bb758e0a6d89efca1f
Chrystelle MORIN <chrystelle.morin@efrei.net>
2022-10-24 23:06:22 +0200

    Create Research/Grade cshtml

---
+++ Etud-Avenir/Etud-Avenir/Controllers/ResearchController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Etud_Avenir.Controllers
+{
+    [Route("Recherche")]
+    public class ResearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/Note")]
+        public IActionResult Grade()
+        {
+            return View();
+        }
+    }
+}
