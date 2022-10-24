888072cc8c5509dadd55c49c8d0bf739c258fe24
Carasaki <nathan.manaranche@gmail.com>
2022-10-24 10:21:34 +0200

    Ajout des premiers styles par défaut

--- Etud-Avenir/Etud-Avenir/Startup.cs
+++ Etud-Avenir/Etud-Avenir/Startup.cs
@@ -34,7 +34,9 @@ namespace Etud_Avenir
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
