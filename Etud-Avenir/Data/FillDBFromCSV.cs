e818baae89b6771fa2a27a0fbc88c3b407bacc01
rhon <rhon.balassiano@hotmail.fr>
2023-02-06 17:03:53 +0100

    tentative fill bdd

---
+++ Etud-Avenir/Data/FillDBFromCSV.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using System.Globalization;
+
+namespace Etud_Avenir.Data
+{
+
+    public class FillDBFromCSV
+    {
+
+        public void ExtractSubject()
+        {
+
+            // path to the csv file
+            string path = "Matierer.csv";
+
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',');
+                foreach (string column in columns)
+                {
+                    // Do something
+                }
+            }
+        }
+
+    }
+}
