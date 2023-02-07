3759fa4def043b9c60a5cacbbf5c6e352e0f5e26
rhon <rhon.balassiano@hotmail.fr>
2023-02-07 15:20:31 +0100

    function matierer

--- Etud-Avenir/Data/FillDBFromCSV.cs
+++ Etud-Avenir/Data/FillDBFromCSV.cs
@@ -5,14 +5,22 @@ using System.Threading.Tasks;
 using System.IO;
 using Microsoft.VisualBasic.FileIO;
 using System.Globalization;
+using Etud_Avenir.Models;
 
 namespace Etud_Avenir.Data
 {
 
     public class FillDBFromCSV
     {
+        private const string Value = "Main Method";
+        private readonly ApplicationDbContext _dbContext;
 
-        public void ExtractSubject()
+        public FillDBFromCSV(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task ExtractSubject()
         {
 
             // path to the csv file
@@ -21,12 +29,19 @@ namespace Etud_Avenir.Data
             string[] lines = System.IO.File.ReadAllLines(path);
             foreach (string line in lines)
             {
-                string[] columns = line.Split(',');
-                foreach (string column in columns)
-                {
-                    // Do something
-                }
+                Subject Subject = new Subject { Name = line };
+
+                await _dbContext.AddAsync(Subject);
+                _dbContext.SaveChanges();
             }
+        }
+
+        static public async Task Main(String[] args)
+        {
+
+            Console.WriteLine(Value);
+            object p = await ExtractSubject();
+
         }
 
     }
